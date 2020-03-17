579b3c7a2a0b9e478762a6566096b7bc3457e8b4
dongju93 <spdlqj011@gmail.com>
2020-03-18 06:24:47 +0900

    3. working on marker

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
-import React, { useState } from 'react';
-import ReactMapGL from 'react-map-gl';
+import React, { useState, useEffect } from 'react';
+import ReactMapGL, { Marker, Popup } from 'react-map-gl';
+import { listLogEntries } from './API';
 
 const App = () => {
+  const [logEntries, setLogEntries] = useState([]);
+  const [showPopup, setShowPopup] = useState({});
   const [viewport, setViewport] = useState({
     width: '100vw',
     height: '100vh',
@@ -10,12 +13,87 @@ const App = () => {
     zoom: 10
   });
 
+  useEffect(() => {
+    (async () => {
+      const logEntries = await listLogEntries();
+      setLogEntries(logEntries);
+    })();
+  }, []);
+
   return (
     <ReactMapGL
       {...viewport}
+      mapStyle="mapbox://styles/tls2323/ck7vszf680xfr1ikboalcsuh2"
       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-      onViewportChange={setViewport}
-    />
+      onViewportChange={setViewport}>
+      {
+        logEntries.map(entry => (
+          <>
+            <Marker
+              key={entry._id}
+              latitude={entry.latitude}
+              longitude={entry.longitude}
+              // offsetLeft={-20}
+              // offsetTop={-10}
+            >
+              <div
+                onClick={() => setShowPopup({
+                  ...showPopup,
+                  [entry._id]: true,
+                })}
+              >
+                <img
+                  style={{
+                    height: `${0.04 * viewport.height}px`,
+                    width: `${0.04 * viewport.width}px`
+                  }}
+                  alt="marker"
+                  className="marker"
+                  src="https://i.imgur.com/y0G5YTX.png"
+                />
+                {/* <svg
+                  // scale svg icon depends on zoom level
+                  style={{
+                    width: `calc(0.5vmin * ${viewport.zoom})`,
+                    height: `calc(0.5vmin * ${viewport.zoom})`
+                  }}
+                  viewBox="0 0 24 24"
+                  stroke="#FFFE00"
+                  strokeWidth="2"
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z">
+                  </path>
+                  <circle cx="12" cy="10" r="3">
+                  </circle>
+                </svg> */}
+              </div>
+            </Marker>
+            {
+              showPopup[entry._id]?(
+                <Popup
+                  latitude={entry.latitude}
+                  longitude={entry.longitude}
+                  closeButton={true}
+                  closeOnClick={true}
+                  onClose={() => setShowPopup({
+                    ...showPopup,
+                    [entry._id]: false
+                  })}
+                  anchor="top" >
+                  <div>
+                    <h3>{entry.title}</h3>
+                    <h5>{entry.comments}</h5>
+                  </div>
+                </Popup>
+              ) : null
+            }
+          </>
+        ))
+      }
+    </ReactMapGL>
   );
 }
 
